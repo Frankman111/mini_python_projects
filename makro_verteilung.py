9ea0c5e241278a03f3d17d40f83ef2677ddeb624
Frankman111 <haider.frank@gmail.com>
2024-10-08 07:33:59 +0200

    makro_verteilung

---
+++ makro_verteilung.py
@@ -0,0 +1,26 @@
+import locale
+locale.setlocale(locale.LC_ALL, 'de_DE')
+
+weight = input("Bitte Gewicht angeben: ")
+target_calories = input("Bitte gewünschte Zielkalorien angeben: ")
+protein_intake = input("Wieviel Protein g/kg Körpergewicht?")
+fat_intake =  input("Wieviel Fett g/kg Körpergewicht?" )
+
+
+def calculations(weight, target_calories, protein_intake, fat_intake):
+    protein = locale.atof(weight)*locale.atof(protein_intake)
+    protein = round(protein, 1)
+    fat = locale.atof(weight)*locale.atof(fat_intake)
+    fat = round(fat, 1)
+    carbs = (locale.atof(target_calories) - ((protein*4.2) + (fat*9))) // 4.2
+    
+    carbs = round(carbs, 1)
+    msg = "Protein: " +  str(protein) +  "g " + "Fett: " + str(fat)  +"g " "Kohlenhydrate: "  + str(carbs) + "g"
+    print(msg)
+    return protein, fat
+    
+
+calculations(weight, target_calories, protein_intake, fat_intake)
+
+
+
