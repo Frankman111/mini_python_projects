fed4f93f718311df6922c6a3b4ae3818b4f03628
Frankman111 <haider.frank@gmail.com>
2024-10-10 15:18:42 +0200

    harris_benedict_calc

--- bmr_calculator.py
+++ bmr_calculator.py
@@ -1,9 +1,6 @@
 import locale
 locale.setlocale(locale.LC_ALL, 'de_DE')
 
-
-
-
 while True:
     
     sex = input("Bitte geben Sie ihr biologisches Geschlecht an: ")
@@ -18,14 +15,16 @@ while True:
     else:
         print("falsche Eingabe")
    
-height = input("Bitte geben Sie ihre Körpergröße (in m) an: ")
+height = input("Bitte geben Sie ihre Körpergröße (in cm) an: ")
 weight = input("Bitte geben Sie ihr Körpergewicht (in Kg) an: ")
+age = input("Bitte geben Sie ihr Alter an: ")
 height = locale.atof(height)
 weight = locale.atof(weight)
+age = locale.atof(age)
 
 # vereinfachte Formel Gewicht*24*1(Mann) oder Gewicht*24*0,9(Frau)
 
-def bmrMale_simple(sex, weight):
+def bmr_simple(sex, weight):
     if sex == "mann":
         simpleCalc = float(weight*24*1)
         print("Ihr Grundumsatz ist bei: "+ str(simpleCalc) + "kcal")
@@ -33,4 +32,12 @@ def bmrMale_simple(sex, weight):
         simpleCalc = float(weight*24*0.9)
         print("Ihr Grundumsatz ist bei: " + str(simpleCalc) + "kcal")
 
-bmrMale_simple(sex, weight)
+bmr_simple(sex, weight)
+
+# Berechnung nach Harris-Benedikt-Formel
+
+def bmr_harris_benedict(sex, weight, height, age):
+    bhCalc = 88.362 + (13.397*float(weight))+(4.799*float(height))-(5.677*float(age))
+    print("Ihr Grundumsatz ist bei: " + str(bhCalc) + "kcal")
+    
+bmr_harris_benedict(sex, weight, height, age)
